b96181575613ba44fa7c24dd7493ace5de74b8e6
saurs saurav <saurssaurav33@gmail.com>
2017-07-05 11:30:46 +0545

    model update when creating or deleting

--- app/controllers/libraries/index.js
+++ app/controllers/libraries/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	queryParams: ['search'],
 	search: '',
-	filteredList: Ember.computed('search', 'model', function(){
+	filteredList: Ember.computed('search', 'model.[]', function(){
 		var search = this.get('search');
 		var libraries = this.get('model');
 		if (libraries) {
