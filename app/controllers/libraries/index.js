5e19cec802ce2d58fbe58d09559acd12c3f3a0ae
saurs saurav <saurssaurav33@gmail.com>
2017-07-05 10:51:36 +0545

    add a search feature in libraries/index

---
+++ app/controllers/libraries/index.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	queryParams: ['search'],
+	search: '',
+	filteredList: Ember.computed('search', 'model', function(){
+		var search = this.get('search');
+		var libraries = this.get('model');
+		if (libraries) {
+	    	const regexString = '(' + search.split(' ').join(')+.*(') + ')+.*';
+			// i: case insensitive, g: global
+			const regex = new RegExp(regexString, 'ig');
+      		var results = libraries.filter((item) => item.get('name').match(regex));
+			return results;
+		} else {
+			return libraries;
+		}
+	})
+	
+});
--- app/routes/libraries/index.js
+++ app/routes/libraries/index.js
@@ -1,7 +1,12 @@
   import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(){
+ queryParams: {
+    search: {
+      refreshModel: true
+    }
+  },
+  model(params){
     return this.store.findAll('library');
   },
   actions:{
